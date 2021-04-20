c52d02f49719eb0137baf78e954845da74ab0d1d
bito-blosh <53908465+bito-blosh@users.noreply.github.com>
2021-04-20 22:22:31 +0900

    Update BarrelsBlockHandler.java

--- src/main/java/me/john000708/barrels/block/BarrelsBlockHandler.java
+++ src/main/java/me/john000708/barrels/block/BarrelsBlockHandler.java
@@ -1,93 +1,83 @@
 package me.john000708.barrels.block;
 
-import org.bukkit.Material;
+import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
+import me.john000708.barrels.items.BarrelItems;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
 import me.john000708.barrels.DisplayItem;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunBlockHandler;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.UnregisterReason;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 
-class BarrelsBlockHandler implements SlimefunBlockHandler {
+import java.util.List;
+
+class BarrelsBlockHandler extends BlockBreakHandler {
 
     private final Barrel barrel;
 
-    public BarrelsBlockHandler(Barrel barrel) {
+    public BarrelsBlockHandler(Barrel barrel, boolean allowAndroids, boolean allowExplosions) {
+        super(allowAndroids, allowExplosions);
         this.barrel = barrel;
     }
 
     @Override
-    public boolean onBreak(Player player, Block b, SlimefunItem slimefunItem, UnregisterReason unregisterReason) {
-        if (unregisterReason.equals(UnregisterReason.EXPLODE)) {
-            if (BlockStorage.getLocationInfo(b.getLocation(), "explosion") != null) {
-                // This Barrel has been protected from explosions
-                return false;
-            }
-        }
-        else if (unregisterReason.equals(UnregisterReason.PLAYER_BREAK)) {
-            // Only the Owner may break this Barrel
-            String owner = BlockStorage.getLocationInfo(b.getLocation(), "owner");
-            if (owner != null && !owner.equals(player.getUniqueId().toString())) {
-                return false;
-            }
+    public void onPlayerBreak(BlockBreakEvent e, ItemStack tool, List<ItemStack> drops) {
+        Player player = e.getPlayer();
+        Block b = e.getBlock();
+
+        // Only the Owner or OP-Player may break this Barrel
+        String owner = BlockStorage.getLocationInfo(b.getLocation(), "owner");
+        if (!player.isOp() && owner != null && !owner.equals(player.getUniqueId().toString())) {
+            e.setCancelled(true);
+            return;
         }
 
         DisplayItem.removeDisplayItem(b);
 
         BlockMenu inv = BlockStorage.getInventory(b);
 
-        if (BlockStorage.getLocationInfo(b.getLocation(), "explosion") != null) {
-            b.getWorld().dropItem(b.getLocation(), SlimefunItem.getByID("BARREL_EXPLOSION_MODULE").getItem());
-        }
 
         if (BlockStorage.getLocationInfo(b.getLocation(), "STRUCT_1") != null) {
-            b.getWorld().dropItem(b.getLocation(), SlimefunItem.getByID("STRUCT_UPGRADE_1").getItem());
+            drops.add(BarrelItems.structUpgrade1);
         }
 
         if (BlockStorage.getLocationInfo(b.getLocation(), "STRUCT_2") != null) {
-            b.getWorld().dropItem(b.getLocation(), SlimefunItem.getByID("STRUCT_UPGRADE_2").getItem());
+            drops.add(BarrelItems.structUpgrade2);
         }
 
         if (BlockStorage.getLocationInfo(b.getLocation(), "STRUCT_3") != null) {
-            b.getWorld().dropItem(b.getLocation(), SlimefunItem.getByID("STRUCT_UPGRADE_3").getItem());
+            drops.add(BarrelItems.structUpgrade3);
         }
 
         if (BlockStorage.getLocationInfo(b.getLocation(), "protected") != null) {
-            b.getWorld().dropItem(b.getLocation(), SlimefunItem.getByID("BARREL_BIO_PROTECTION").getItem());
+            drops.add(BarrelItems.biometricProtectionModule);
         }
 
-        inv.dropItems(b.getLocation(), barrel.getInputSlots());
-        inv.dropItems(b.getLocation(), barrel.getOutputSlots());
+        for (int slot : barrel.getInputSlots()) {
+            drops.add(inv.getItemInSlot(slot));
+        }
+        for (int slot : barrel.getOutputSlots()) {
+            drops.add(inv.getItemInSlot(slot));
+        }
 
         String storedAmountString = BlockStorage.getLocationInfo(b.getLocation(), "storedItems");
         if (storedAmountString == null) {
-            return true;
+            return;
         }
         int storedAmount = Integer.parseInt(storedAmountString);
 
-        ItemStack item = inv.getItemInSlot(22);
+        ItemStack itemStored = inv.getItemInSlot(22);
 
         while (storedAmount > 0) {
-            int amount = item.getMaxStackSize();
+            int amount = Math.min(storedAmount, itemStored.getMaxStackSize());
+            storedAmount -= amount;
 
-            if (storedAmount > amount) {
-                storedAmount -= amount;
-            }
-            else {
-                amount = storedAmount;
-                storedAmount = 0;
-            }
-
-            b.getWorld().dropItem(b.getLocation(), new CustomItem(item, amount));
+            drops.add(new CustomItem(itemStored, amount));
         }
         BlockStorage.addBlockInfo(b, "storedItems", null);
-        inv.replaceExistingItem(22, new CustomItem(Material.BARRIER, "&7Empty"), false);
-
-        return true;
     }
+
 }
